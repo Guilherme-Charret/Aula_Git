ae3d3e792acad99412762f4e178ac7d68c9b8b60
Guilherme-Charret <guilhermecharretdevasconcellos@gmail.com>
2025-07-15 13:29:37 -0300

    Exemplo

---
+++ exemplo.ts
@@ -0,0 +1,32 @@
+const sum = 2 + 3
+
+const authorized = true
+const unauthorized = true
+
+if (!authorized){
+    "faz algo"
+}
+
+if (unauthorized){
+    "faz algo"
+}
+
+function createVariable(){
+    const x = 0;
+    const y = 1; 
+    const z = x + y + 1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//git add .
+//git commit -m "mensagem"
